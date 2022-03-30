c8479de1d0626acd8acf736a75b90f1f34889c32
abi <abilalu1994@gmail.com>
2022-03-29 21:59:55 -0500

    updating files from local

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route,Routes } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Home from "./components/pages/Home";
+import Projects from "./components/pages/Projects";
+import Contact from "./components/pages/Contact";
+import HeroImage from "./components/HeroImage";
+import FooterLine from "./components/FooterLine"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router basename={ process.env.PUBLIC_URL }>
+      <div>
+        <HeroImage/>
+        <NavBar />
+        <Routes>
+        <Route exact path="/" element={<Home/>} />
+        <Route exact path="/Projects" element={<Projects/>} />
+        <Route path="/contact" element={<Contact/>} />
+        </Routes>
+        <FooterLine/>
+      </div>
+    </Router>
   );
 }
 
